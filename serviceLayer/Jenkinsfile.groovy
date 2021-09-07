ff525de616da07447750620c9ad2c1cf7f7fa929
navaneethan2 <naveenhari55@gmail.com>
2021-09-07 15:32:03 +0530

    001 new change

--- serviceLayer/Jenkinsfile.groovy
+++ serviceLayer/Jenkinsfile.groovy
@@ -2,10 +2,8 @@ pipeline{
     agent any
     stages{
         stage('check change'){
-            when {changeset "*/APIParent/**"}
             steps{
                 sh "echo I will build now"
-                build job:'orc'
             }
         }
     }
