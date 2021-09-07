b506c9bcb0534da9994f8572eec93634a2e156fa
navaneethan2 <naveenhari55@gmail.com>
2021-09-07 16:51:06 +0530

    001 new change

--- serviceLayer/APIParent/jenkinsfile.groovy
+++ serviceLayer/APIParent/jenkinsfile.groovy
@@ -5,7 +5,10 @@ pipeline{
             when {changeset "*/APIParent/**"}
             steps{
                 sh "echo I will build now"
-                build job:'orc'
+                build (job:'orc',parameters: [
+                        [$class: 'StringParameterValue', name: 'PROJECT', value: env.PROJECT],
+                        [$class: 'StringParameterValue', name: 'JOB_BASE_NAME', value: env.JOB_BASE_NAME]
+                ])
             }
         }
     }
--- serviceLayer/Jenkinsfile.groovy
+++ serviceLayer/Jenkinsfile.groovy
@@ -1,9 +1,19 @@
 pipeline{
     agent any
+
+    parameters{
+        string(name:'project', defaultvalue:'serviceLayer',//"${}")
+
+    }
     stages{
         stage('check change'){
             steps{
+                script{
+                    //println("project- ${params.PROJECT}")
+                    println("project- ${PROJECT}")
+                }
                 sh "echo I will build now"
+
             }
         }
     }
