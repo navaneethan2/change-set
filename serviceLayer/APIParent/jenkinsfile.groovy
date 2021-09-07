d93afa92f267adf0441d29698e6f2499deef98db
navaneethan2 <naveenhari55@gmail.com>
2021-09-07 15:02:15 +0530

    001 new change

--- serviceLayer/APIParent/jenkinsfile.groovy
+++ serviceLayer/APIParent/jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline{
     agent any
     stages{
         stage('check change'){
-            when {changeset "APIParent"}
+            when {changeset "*/APIParent/**"}
             steps{
                 sh "echo I will build now"
             }
