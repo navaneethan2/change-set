9ab229000d4c522d27a56e008892a7b2cac421f0
navaneethan2 <naveenhari55@gmail.com>
2021-09-13 00:31:18 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- scripts/modules.groovy
+++ scripts/modules.groovy
@@ -1,6 +1,6 @@
 def getListModules(String modulesFile){
 
-    return sh(script: "cat ${modulesFile}", returnStdout: true)
+    return sh(script: "cat ${modulesFile} | jq -r .'repositories.sublists[] | .repos[].name'", returnStdout: true)
 }
 
 return this;
