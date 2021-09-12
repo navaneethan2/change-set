df8cd67b8947fe4b420e377e804af46ed4273cdd
navaneethan2 <naveenhari55@gmail.com>
2021-09-12 21:56:33 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- API/jenkinsfile1.groovy
+++ API/jenkinsfile1.groovy
@@ -25,7 +25,7 @@ pipeline{
                 steps{
                     script{
                         sh "echo i will build now, and i have a latest commit in ${params.API_to_build}"
-                        sh "cd ${env.WORKSPACE}"
+                        sh "cd ${env.WORKSPACE} && ls -ltr"
                     }
 
                 }
