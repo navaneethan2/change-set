fd831735c611168170903a38f382eb59c328c1c8
navaneethan2 <naveenhari55@gmail.com>
2021-09-12 22:19:02 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- API/jenkinsfile1.groovy
+++ API/jenkinsfile1.groovy
@@ -1,29 +1,31 @@
-pipeline{
+
+pipeline {
 
     agent any
 
-    parameters{
+    parameters {
 
-        choice(name:'API_to_build',choices: ['AlertsAPI','CustomerAPI'])
+        choice(name: 'API_to_build', choices: ['AlertsAPI', 'CustomerAPI'])
     }
 
-    stages{
+    stages {
 
-        stage('debug'){
-            steps{
-                script{
+        stage('debug') {
+            steps {
+                script {
                     println "${params.ParamA}"
                     println "${params.API_to_build}"
                 }
             }
         }
-        stage('changeset'){
+        stage('changeset') {
             when {
-                changeset "*/${params.API_to_build}/**"
-                //expression { params.ParamA == 'Orchestration' }
-            }
-                steps{
-                    script{
+                allOf {
+                    changeset "*/${params.API_to_build}/**"
+                    expression { params.ParamA == 'Orchestration' }
+                }
+                steps {
+                    script {
                         sh "echo i will build now, and i have a latest commit in ${params.API_to_build}"
                         sh "cd ${env.WORKSPACE} && cd API/${params.API_to_build}/ && ls"
                     }
@@ -32,3 +34,5 @@ pipeline{
             }
         }
     }
+
+}
