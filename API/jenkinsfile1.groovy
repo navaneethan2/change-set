c1a86891150490556a679eae4e29bec47296f3ae
navaneethan2 <naveenhari55@gmail.com>
2021-09-12 21:26:34 +0530

    0002 - Added API, AlertsAPI jenkinsfile

---
+++ API/jenkinsfile1.groovy
@@ -0,0 +1,34 @@
+pipeline{
+
+    agent any
+
+    parameters{
+
+        choice(name:'API_to_build',choices: ['AlertsAPI','CustomerAPI'])
+    }
+
+    stages{
+
+        stage('debug'){
+            steps{
+                script{
+                    println "${params.ParamA}"
+                    println "${params.API_to_build}"
+                }
+            }
+        }
+        stage('changeset'){
+            when { allOf {
+                changeset "*/${params.API_to_build}/**"
+                //expression { params.ParamA == 'Orchestration' }
+            }
+                steps{
+                    script{
+                        sh "echo i will build now, and i have a latest commit in ${params.API_to_build}"
+                    }
+
+                }
+            }
+        }
+    }
+}
