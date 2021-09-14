c6e2963e919022a6448a7e1d1f5d1ef5e289ee86
navaneethan2 <naveenhari55@gmail.com>
2021-09-14 15:29:25 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- jenkinsfileOrch1.groovy
+++ jenkinsfileOrch1.groovy
@@ -20,6 +20,7 @@ pipeline {
 
                     def API_modules = ['AlertsAPI', 'CustomerAPI']
 
+
                     for (int i = 0; i < API_modules.size(); ++i) {
                         echo "${API_modules[i]}"
                         echo "build job: 'test1',parameters: [ string:(name: 'ParamA', value: paramAValue),(string(name: 'API_to_build', value: '${API_modules[i]}'))]"
