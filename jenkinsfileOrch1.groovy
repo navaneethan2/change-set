2432add615bdd728c93a08d3562812c1826a1e1a
navaneethan2 <naveenhari55@gmail.com>
2021-09-14 14:39:48 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- jenkinsfileOrch1.groovy
+++ jenkinsfileOrch1.groovy
@@ -1,21 +1,20 @@
 def paramAValue = "Orchestration"
 
 
-pipeline{
+pipeline {
 
     agent any
 
-    stages{
+    stages {
 
-        }
         stage('pass parameter') {
 
-            steps{
-                script{
+            steps {
+                script {
 
-                    def API_modules = ['AlertsAPI','CustomerAPI']
+                    def API_modules = ['AlertsAPI', 'CustomerAPI']
 
-                    for (int i = 0; i < API_modules.size(); ++i){
+                    for (int i = 0; i < API_modules.size(); ++i) {
                         echo "${API_modules[i]}"
                         echo "build job: 'test1',parameters: [ string:(name: 'ParamA', value: paramAValue),(string(name: 'API_to_build', value: '${API_modules[i]}'))]"
 
@@ -24,7 +23,7 @@ pipeline{
 
                 }
             }
+        }
 
-}
-
+    }
 }
