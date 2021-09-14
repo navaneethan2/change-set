a4119c923c765e9c72e7a32738e4c56688707325
navaneethan2 <naveenhari55@gmail.com>
2021-09-14 15:51:32 +0530

    0002 - Added API, AlertsAPI jenkinsfile

--- jenkinsfileOrch1.groovy
+++ jenkinsfileOrch1.groovy
@@ -1,4 +1,11 @@
-
+@NonCPS
+def echo_all(moduleName) {
+    moduleName.each { item ->
+        echo "Hello ${item}"
+        echo "build job: 'test1',parameters: [ string(name: 'ParamA', value: paramAValue),(string(name: 'orch_Api', value: moduleName))]"
+        build job: 'test1',parameters: [ [$class: 'StringParameterValue',name: 'ParamA', value: paramAValue],(string(name: 'orch_Api', value: moduleName))]
+    }
+}
 
 def buildJob(moduleName){
     def paramAValue = "Orchestration"
@@ -19,13 +26,13 @@ pipeline {
                 script {
 
                     def API_modules = ['AlertsAPI', 'CustomerAPI']
+                    echo_all(API_modules)
 
-
-                    for (int i = 0; i < API_modules.size(); ++i) {
+                    /*for (int i = 0; i < API_modules.size(); ++i) {
                         echo "${API_modules[i]}"
                         echo "build job: 'test1',parameters: [ string:(name: 'ParamA', value: paramAValue),(string(name: 'API_to_build', value: '${API_modules[i]}'))]"
                         buildJob("${API_modules[i]}")
-                    }
+                    }*/
 
 
                 }
